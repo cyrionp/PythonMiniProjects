61293e53d903cc24277d2c57d85e2c15c25f2695
cyrionpow <qadrrdrmz@gmail.com>
2020-09-14 14:56:31 +0300

    typo

--- 03_Max_and_Min.py
+++ 03_Max_and_Min.py
@@ -1,4 +1,4 @@
-numbers=input("Input numbers with spaces: ")
+numbers=input("Input integers with spaces: ")
 listNumbers=numbers.split(" ")
 listNumbers.sort()
 print(f"Max: {listNumbers[-1]} Min: {listNumbers[0]}")
